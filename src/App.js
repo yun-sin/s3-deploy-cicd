7c1ccbfa334d1ef7187ccdf8f4f06cefc46ecaad
장윤신 <99275134+yun-sin@users.noreply.github.com>
2023-07-04 13:24:42 +0900

    actions deploy test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>npm run build && aws s3 sync build s3://wanted-internship-yun-sin --delete --profile wanted</h1>
       <h2>package</h2>
+      <h2>github actions</h2>
     </div>
   );
 }
